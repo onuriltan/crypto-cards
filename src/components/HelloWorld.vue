bcf7466753b3f5d5ec81f2e85843949ecf3de52b
onuriltan <onur.iltan@gmail.com>
2021-02-27 12:54:41 +0300

    feat: initialize project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      This project will show extendible crypto card components that you can moniyor markets<br>
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+
+</style>
